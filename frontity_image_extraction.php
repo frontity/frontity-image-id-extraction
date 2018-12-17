8a224308b2d5c9ead05e6def49e09dc949cb952c
Eduardo Campaña <asrielo@gmail.com>
2018-12-17 16:52:51 +0100

    Initial commit

---
+++ frontity_image_extraction.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Frontity Image Extraction
+Plugin URI: 
+Description: Adds attribute data-attachment-id to images in content.
+Version: 0.1.0
+Author: Frontity
+Author URI: https://frontity.com/
+License: GPL v3
+Copyright: Worona Labs SL
+ */
